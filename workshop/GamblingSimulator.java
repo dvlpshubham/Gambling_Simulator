570da098163d58d6f990cce376f4468268f6a5af
Shubham Raj <shubhamaryan805@gmail.com>
2022-08-25 13:49:10 +0530

    Added : Gambler Resigns if he win or loss 50% of his stake

--- workshop/GamblingSimulator.java
+++ workshop/GamblingSimulator.java
@@ -3,27 +3,35 @@ package workshop;
 import java.util.Random;
 
 public class GamblingSimulator {
+    public static final int stakePerDay = 100;
+    public static final int betPerGame = 1;
+    public static final int win = 1;
+    public static int stake = 0;
     public static void winCheck() {
-        int stakePerDay = 100;
-        int betPerGame = 1;
-        int win = 1;
-        int stake = 0;
         Random random = new Random();
         int betReturns = random.nextInt(2);
         if (betReturns == win) {
             stake++;
-            System.out.println("win");
         } else {
             stake--;
-            System.out.println("loss");
         }
         System.out.println("stake = " + stake);
 
     }
 
+    public static void resignDayCheck() {
+        int totalStake = 0;
+        while (stake != 50 && stake != -50) {
+            winCheck();
+        }
+        totalStake = stake + stake;
+        System.out.println("Total stake for resign for a day is  " + totalStake);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation");
-        winCheck();
+        resignDayCheck();
     }
 
 }
