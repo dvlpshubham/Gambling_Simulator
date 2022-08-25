e23e31d72d63cf49bc14b590af411cbac9a9b13a
Shubham Raj <shubhamaryan805@gmail.com>
2022-08-25 13:36:58 +0530

    Added : Initial Stake and bet values

---
+++ workshop/GamblingSimulator.java
@@ -0,0 +1,14 @@
+package workshop;
+
+public class GamblingSimulator {
+
+    public static void main(String[] args) {
+        int stake = 100;
+        int bet =  1;
+        System.out.println("Welcome To Gambling Simulator");
+        System.out.println("Gambler start with stake of $" + stake + " every day");
+        System.out.println("Gambler bet for $" + bet + " every game");
+    }
+
+
+}
